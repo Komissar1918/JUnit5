9c5100c63d6d34b8e6ab16022219e501b99065d5
Yosyf Akhmetov <Avtograd-Rabota2020@yandex.ru>
2023-04-13 17:14:23 +0300

    III/Assertj & Hamcrest

--- src/main/java/com/komissar/junit/service/UserService.java
+++ src/main/java/com/komissar/junit/service/UserService.java
@@ -3,7 +3,13 @@ package com.komissar.junit.service;
 import com.komissar.junit.dto.User;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toMap;
 
 public class UserService {
     private final List<User> users = new ArrayList<>();
@@ -11,7 +17,19 @@ public class UserService {
         return users;
     }
 
-    public boolean add(User user) {
-        return users.add(user);
+    public void add(User...users) {
+        this.users.addAll(Arrays.asList(users));
+    }
+
+    public Optional<User> login(String username, String password) {
+        return users.stream()
+                .filter(user -> user.getUsername().equals(username))
+                .filter(user -> user.getPassword().equals(password))
+                .findFirst();
+    }
+
+    public Map<Integer, User> getAllConvertedById() {
+        return users.stream()
+                .collect(toMap(User::getId, Function.identity()));
     }
 }
--- src/test/java/com/komissar/junit/service/UserServiceTest.java
+++ src/test/java/com/komissar/junit/service/UserServiceTest.java
@@ -3,18 +3,27 @@ package com.komissar.junit.service;
 import com.komissar.junit.dto.User;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import java.util.List;
 
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
  class UserServiceTest {
     private UserService userService;
+    private static final User YOSYF =User.of(1,"Yosyf","Rammstein2012");
+    private static final User PASHA = User.of(2,"Pavel","123");
 
     @BeforeAll //используются для указания о том, что аннотированный метод должен быть выполнен перед всеми @Test, @RepeatedTest, @ParameterizedTest и @TestFactory методами в текущем классе.
     static void init() {
@@ -35,15 +44,52 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
         assertTrue(users.isEmpty());  //тип assert - boolean.
         // input ->[box == func] -> actual output
     }
-
     @Test
     void usersSizeIfUserAdded() {
         System.out.println("Test 2: " + this);
         userService = new UserService();
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(YOSYF);
+        userService.add(PASHA);
         List<User> users = userService.getAll();
-        assertEquals(2, users.size());
+        assertThat(users).hasSize(2);
+//        assertEquals(2, users.size());
+    }
+
+    @Test
+    void userSizeIfUserAdded(){
+        System.out.println("Test 3: " + this);
+        userService.add(YOSYF);
+        Optional<User> maybeUser = userService.login(YOSYF.getUsername(),YOSYF.getPassword());
+        assertThat(maybeUser).isPresent();
+        maybeUser.ifPresent(user -> assertThat(user).isEqualTo(YOSYF));
+//        assertTrue(maybeUser.isPresent());
+//        maybeUser.ifPresent(user -> assertEquals(YOSYF,user));
+
+    }
+    @Test
+    void loginFailIfPasswordIsNotCorrect(){
+        System.out.println("Test 4: " + this);
+        userService.add(YOSYF);
+        Optional<User>maybeUser = userService.login(YOSYF.getUsername(),"dummy");
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfUserDoesNotExist(){
+        System.out.println("Test 5: " + this);
+        userService.add(YOSYF);
+        Optional<User> maybeUser = userService.login("dummy", YOSYF.getPassword());
+        assertTrue(maybeUser.isEmpty());
+
+    }
+    @Test
+    void usersConvertedToMapById(){
+        userService.add(YOSYF, PASHA);
+       Map<Integer,User> userMap =  userService.getAllConvertedById();
+       assertAll(
+               ()->assertThat(userMap).containsKeys(YOSYF.getId(),PASHA.getId()),
+               ()->assertThat(userMap).containsValues(YOSYF,PASHA)
+       );
     }
 
     @AfterEach //используется для обозначения того, что аннотированный метод должен выполняться после каждого метода, помеченного @Test, @RepeatedTest, @ParameterizedTest, или @TestFactory в текущем классе
